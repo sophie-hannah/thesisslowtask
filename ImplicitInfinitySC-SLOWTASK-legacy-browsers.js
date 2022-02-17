34df058818fb5febf5eba688cfcd40419cca2172
Sophie Cramer <scramerb@skidmore.edu>
2022-02-17 13:16:36 -0500

    0

--- ImplicitInfinitySC-SLOWTASK-legacy-browsers.js
+++ ImplicitInfinitySC-SLOWTASK-legacy-browsers.js
@@ -63,9 +63,9 @@ psychoJS.start({
   expName: expName,
   expInfo: expInfo,
   resources: [
-    {'name': 'SlowInstructionsSheet2.xlsx', 'path': 'SlowInstructionsSheet2.xlsx'},
+    {'name': 'InfinityThesisStimuli.xlsx', 'path': 'InfinityThesisStimuli.xlsx'},
     {'name': 'InfinityThesisPracticeStimuli.xlsx', 'path': 'InfinityThesisPracticeStimuli.xlsx'},
-    {'name': 'InfinityThesisStimuli.xlsx', 'path': 'InfinityThesisStimuli.xlsx'}
+    {'name': 'SlowInstructionsSheet2.xlsx', 'path': 'SlowInstructionsSheet2.xlsx'}
   ]
 });
 
